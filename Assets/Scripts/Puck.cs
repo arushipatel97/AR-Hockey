6d3611ed4dcd433f8b4f0f57d329bad265580998
Arushi <arushipatel97@gmail.com>
2019-04-09 01:59:11 -0400

    Initial commit

---
+++ Assets/Scripts/Puck.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puck : MonoBehaviour
+{
+
+    public delegate void GoalHandler();
+    public event GoalHandler OnGoal;
+
+    public float deceleration;
+    public float startingHorizontalPosition;
+
+    private Rigidbody puckRigidbody;
+    Vector3 startPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        puckRigidbody = gameObject.GetComponent<Rigidbody>();
+        transform.position = new Vector3(
+            startingHorizontalPosition * -1,
+            transform.position.y,
+            transform.position.z
+        );
+        startPos = transform.position;
+        //ResetPosition(true);
+    }
+
+    void FixedUpdate()
+    {
+        puckRigidbody.velocity = new Vector3(
+            puckRigidbody.velocity.x * deceleration,
+            puckRigidbody.velocity.y * deceleration,
+            puckRigidbody.velocity.z * deceleration
+        );
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Goal")
+        {
+            if (OnGoal != null)
+            {
+                OnGoal();
+            }
+        }
+        else
+        {
+         //   gameObject.GetComponent<AudioSource>().Play();
+        }
+    }
+
+    public void ResetPosition(bool isLeft)
+    {
+        transform.position = startPos;
+
+        puckRigidbody.velocity = Vector3.zero;
+    }
+}
